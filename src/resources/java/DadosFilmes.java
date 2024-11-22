b38cb8a6bc58e0046c5b25745fe6dda8c4755aff
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-11-22 16:42:09 -0300

    First commit

---
+++ src/resources/java/DadosFilmes.java
@@ -0,0 +1,46 @@
+package resources.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DadosFilmes {
+    public static void main(String[] args) {
+        String caminhoDoArquivo =
+                "C:\\Users\\matheus.fgs\\Desktop\\The_Horror" +
+                        "\\Banco_de_Dados\\Info_Filmes.txt";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do filme que deseja buscar: ");
+        String nomeDoFilme = scanner.nextLine();
+        scanner.close();
+
+        boolean filmeEncontrado = false;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoDoArquivo))) {
+            String linha;
+            System.out.println("\nBuscando por: " + nomeDoFilme + "...\n");
+
+            while ((linha = br.readLine()) != null) {
+                if (linha.toLowerCase().contains(nomeDoFilme.toLowerCase())) {
+                    System.out.println("Filme encontrado:");
+                    System.out.println(linha);
+                    filmeEncontrado = true;
+
+                    for (int i = 0; i < 3; i++) {
+                        linha = br.readLine();
+                        if (linha != null) {
+                            System.out.println(linha);
+                        }
+                    }
+                    break;                }
+            }
+            if (!filmeEncontrado) {
+                System.out.println("O filme" + nomeDoFilme + " não foi encontrado no arquivo");
+            }
+        }catch (IOException e) {
+            System.out.println("Erro ao acessar o arquivo: " + e.getMessage());
+        }
+    }
+}
---
+++ src/resources/java/TelaInicial.java
@@ -0,0 +1,70 @@
+package resources.java;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TelaInicial extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("THE HORROR");
+
+        BorderPane folclore = new BorderPane();
+        /*
+        folclore.setStyle("-fx-background-image: url('" + getClass().getResource(
+                "resources/Images/Background.gif").toExternalForm() + "');" + "-fx-background-size: cover;");
+         */
+
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(20));
+
+        Label tituloLabel = new Label("SEJA BEM VINDO(A)");
+        tituloLabel.setId("titulo-label");
+        vbox.getChildren().add(tituloLabel);
+
+        Region space1 = new Region();
+        space1.setPrefHeight(100);
+        vbox.getChildren().add(space1);
+
+        Button iniciar = new Button("INICIAR");
+        iniciar.setId("iniciar-button");
+        vbox.getChildren().add(iniciar);
+
+        iniciar.setOnAction(e -> {});
+
+        Button sobre = new Button("SOBRE NÓS");
+        sobre.setId("sobre-button");
+        vbox.getChildren().add(sobre);
+
+        sobre.setOnAction(e -> {});
+
+        folclore.setCenter(vbox);
+
+        HBox footer = new HBox(10);
+        footer.setAlignment(Pos.CENTER);
+        footer.setPadding(new Insets(10));
+        footer.setId("footer");
+
+        Label rodapeLabel = new Label("By Matheus Ferreira");
+        rodapeLabel.setId("rodape");
+
+        footer.getChildren().addAll(rodapeLabel);
+        folclore.setBottom(footer);
+
+        Scene mainScene = new Scene(folclore, 900, 540);
+        mainScene.getStylesheets().add(getClass().getResource("/resources/Style/TelaInicial.css").toExternalForm());
+
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+}
