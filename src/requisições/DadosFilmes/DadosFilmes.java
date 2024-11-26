eef558917bb48040a87725a75577bf0f01bca9c8
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-11-26 16:56:38 -0300

    Ajustes do banco de dados dos Filmes

--- src/requisições/DadosFilmes/DadosFilmes.java
+++ src/requisições/DadosFilmes/DadosFilmes.java
@@ -1,6 +1,7 @@
 package requisições.DadosFilmes;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,36 +11,44 @@ public class DadosFilmes {
         String caminhoDoArquivo = "C:\\Users\\matheus.fgs\\Desktop\\The-Horror\\src" +
                 "\\Banco_de_Dados\\Info_Filmes.txt";
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o nome do filme que deseja buscar: ");
-        String nomeDoFilme = scanner.nextLine();
-        scanner.close();
-
         boolean filmeEncontrado = false;
 
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoDoArquivo))) {
             String linha;
-            System.out.println("\nBuscando por: " + nomeDoFilme + "...\n");
+            String diretor = "";
+            String filme = "";
+            String descricao = "";
+            String ano_filme = "";
+            String caminho_imagem = "";
 
-            while ((linha = br.readLine()) != null) {
-                if (linha.toLowerCase().contains(nomeDoFilme.toLowerCase())) {
-                    System.out.println("Filme encontrado:");
-                    System.out.println(linha);
-                    filmeEncontrado = true;
+            while ((linha = br.readLine()) != null){
 
-                    for (int i = 0; i < 3; i++) {
-                        linha = br.readLine();
-                        if (linha != null) {
-                            System.out.println(linha);
-                        }
-                    }
-                    break;                }
+                if (linha.startsWith("- Diretor:")) {
+                    diretor = linha.substring(9).trim();
+
+                } else if (linha.startsWith("- Filme:")) {
+                    filme = linha.substring(7).trim();
+
+                } else if (linha.startsWith("- Descrição:")) {
+                    descricao = linha.substring(11).trim();
+
+                }else if (linha.startsWith("- Imagem:")){
+                    caminho_imagem = linha.substring(11).trim();
+
+                }else if (linha.startsWith("- Ano:")){
+                    caminho_imagem = linha.substring(4).trim();{
+
+                    System.out.println("Diretor: " + diretor);
+                    System.out.println("Filme: " + filme);
+                    System.out.println("Descrição: " + descricao);
+                    System.out.println("Imagem : " + caminho_imagem);
+                    System.out.println("Ano : " + ano_filme);
+                    System.out.println("-------------");
+                }
             }
-            if (!filmeEncontrado) {
-                System.out.println("O filme" + nomeDoFilme + " não foi encontrado no arquivo");
             }
-        }catch (IOException e) {
-            System.out.println("Erro ao acessar o arquivo: " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
